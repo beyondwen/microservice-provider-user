b82e371eff32af6b9ff3f0ff652b471dbbdc80d0
wenhao176 <921790476@qq.com>
2018-12-07 15:26:47 +0800

    添加认证

--- src/main/java/com/wenhao/microservice/microserviceprovideruser/controller/UserController.java
+++ src/main/java/com/wenhao/microservice/microserviceprovideruser/controller/UserController.java
@@ -3,10 +3,17 @@ package com.wenhao.microservice.microserviceprovideruser.controller;
 
 import com.wenhao.microservice.microserviceprovideruser.dao.UserRepository;
 import com.wenhao.microservice.microserviceprovideruser.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class UserController {
@@ -14,8 +21,18 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/{id}")
     public User findById(@PathVariable Long id) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            UserDetails user = (UserDetails) principal;
+            Collection<? extends GrantedAuthority> collection = user.getAuthorities();
+            for (GrantedAuthority grantedAuthority : collection) {
+
+            }
+        }
         User user = this.userRepository.findOne(id);
         return user;
     }
